e97208d5e1de75482dd18d0497d4fcb1ad45abb8
egel <egel@dev.al>
2026-01-28 11:14:35 +0100

    working clean

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,9 @@
-import React from "react"
-import {View, Text, Image, ScrollView, TextInput} from 'react-native';
+import {View, Text} from 'react-native';
 
 const App = () =>{
 return(
     <View>
-    <Text>Welcome to React Native!</Text>
+    <Text>Working demo React Native!</Text>
 </View>
 )
 }
