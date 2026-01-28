d0d5ad3f3e1f2191c328ac81c286bfe93131f180
egel <egel@dev.al>
2026-01-28 12:23:13 +0100

    component task

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,35 @@
 import {View, Text} from 'react-native';
 
-const App = () =>{
-return(
-    <View>
-    <Text>Working demo React Native!</Text>
-</View>
-)
+type WelcomeCardProps = {
+    title: string;
+    subtitle: string;
 }
 
-export default App;
+const WelcomeCard = ({title, subtitle}) =>{
+    return <View style = {styles.container}>
+        <Text> TITLE : {title} {" "} : {"\n"} {subtitle}</Text>
+    </View>;
+}
+
+const HomeScreen = () =>{
+return <View>
+    <WelcomeCard title="Welcome card 1" subtitle="this is card 1"/>
+    <WelcomeCard title="Welcome card 2" subtitle="this is card 2"/>
+    </View>
+}
+
+const styles = {
+    container: {
+        marginTop: 50,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 5,
+        backgroundColor: '#f0f0f0',
+        marginBottom: 10,
+        marginLeft: 10,
+        marginRight: 10,
+    },
+};
+
+export default HomeScreen;
